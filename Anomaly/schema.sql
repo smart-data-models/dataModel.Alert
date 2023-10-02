bd99f8571be78f8126f1281768bb60d1c9127afb
Alberto Abella <alberto.abella@fiware.org>
2023-10-02 10:32:15 +0200

    test of SQL export

--- Anomaly/schema.sql
+++ Anomaly/schema.sql
@@ -1,3 +1,3 @@
-/* (Beta) Export of data model Anomaly of the subject dataModel.Alert for a postgreSQL database. Pending translation of enumerations and multityped attributes */
+/* (Beta) Export of data model Anomaly of the subject dataModel.Alert for a PostgreSQL database. Pending translation of enumerations and multityped attributes */
 CREATE TYPE Anomaly_type AS ENUM ('Anomaly');
-CREATE TABLE Anomaly (address json, alternateName text, anomalousProperty text, areaServed text, dataProvider text, dateCreated timestamp, dateDetected text, dateModified timestamp, description text, detectedBy text, id text, location json, name text, owner json, seeAlso json, source text, thresholdBreach json, type Anomaly_type);
+CREATE TABLE Anomaly (address JSON, alternateName TEXT, anomalousProperty TEXT, areaServed TEXT, dataProvider TEXT, dateCreated TIMESTAMP, dateDetected TEXT, dateModified TIMESTAMP, description TEXT, detectedBy TEXT, name TEXT, owner JSON, source TEXT, thresholdBreach JSON, type Anomaly_type);
